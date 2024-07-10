624066a11ff512b3caca597490ab06dd80b39743
molinacarlosj19 <84748598+molinacarlosj19@users.noreply.github.com>
2024-07-10 15:34:10 -0400

    Update for Search By Product Code
    Update for Search By Product Code

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,15 @@
+// routes/productRoutes.js
 const express = require('express');
 const ProductController = require('../controllers/ProductController');
 
 const router = express.Router();
 const productController = new ProductController();
 
+router.get('/search', (req, res, next) => productController.searchProducts(req, res, next));
 router.post('/', (req, res, next) => productController.createProduct(req, res, next));
 router.get('/', (req, res, next) => productController.getAllProducts(req, res, next));
 router.get('/:id', (req, res, next) => productController.getProductById(req, res, next));
 router.put('/:id', (req, res, next) => productController.updateProduct(req, res, next));
 router.delete('/:id', (req, res, next) => productController.deleteProduct(req, res, next));
-router.get('/search', (req, res, next) => productController.searchProducts(req, res, next));
 
 module.exports = router;
